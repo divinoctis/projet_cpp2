4f6b7f361be178d99f5e5604f879b8f1680cde5d
Zeno <mdubos@gaming.tech>
2024-10-18 00:29:38 +0200

    import

--- Sprite.h
+++ Sprite.h
@@ -1,20 +1,3 @@
-//#ifndef SPRITE_H__
-//#define SPRITE_H__
-//
-//
-//class Sprite
-//{
-//public:
-//	virtual ~Sprite();
-//	virtual void setPosition(float x, float y) = 0;
-//	virtual void drawCircle(float x, float y, float radius) = 0;
-//};
-//
-//
-//#endif // !SPRITE_H__
-
-// post vector 2
-
 #ifndef SPRITE_H__
 #define SPRITE_H__
 
